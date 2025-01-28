686f4a3ef1a2f26b7d6a94bccecf6c22a3e02448
RedginaldGodeau <redginaldgodeau95@gmail.com>
2025-01-28 11:26:57 +0100

    feat: adding task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+import debounce from 'lodash/debounce';
+
+const elements = [
+    "<p>Holberton Dashboard</p>",
+    "<p>Dashboard data for the students</p>",
+    "<button>Click here to get started</button>",
+    "<p id='count'></p>",
+    "<p>Copyright - Holberton School</p>"
+];
+
+let count = 0;
+
+function updateCounter() {
+    $("#count").text(++count + " clicks on the button")
+}
+
+$("document").ready(() => {
+    const debouncedClick = debounce(updateCounter, 500);
+    $("body").append(elements.map((v, i) => v).join(""));
+    $("button").click(debouncedClick)
+})
