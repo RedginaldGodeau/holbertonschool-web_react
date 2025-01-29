899124ebe8a121baaa8bcc71dded6216a92cc3e2
RedginaldGodeau <redginaldgodeau95@gmail.com>
2025-01-29 10:05:39 +0100

    fix: simplify webpack config

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,15 +1,10 @@
 const path = require('path');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-
 module.exports = {
     entry: './js/dashboard_main.js',
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
     },
     module: {
         rules: [
@@ -18,39 +13,10 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][hash][ext]',
-                },
             },
         ],
     },
-    optimization: {
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizerPlugin(),
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminGenerate,
-                    options: {
-                        plugins: [
-                            ['imagemin-mozjpeg', {quality: 75}],
-                            ['imagemin-pngquant', {quality: [0.6, 0.8]}],
-                            ['imagemin-svgo', {}],
-                        ],
-                    },
-                },
-            }),
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-    ],
-    resolve: {
-        extensions: ['.js', '.css'],
-    },
-    mode: 'production', // Changer en 'development' pour le développement
+    mode: 'production',
 };
