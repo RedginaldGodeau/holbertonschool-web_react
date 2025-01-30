070f8d93dd5125f1634c81b229ebcef7522db472
RedginaldGodeau <redginaldgodeau95@gmail.com>
2025-01-30 09:31:49 +0100

    fix: adding webpacks loaders package

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,22 +1,36 @@
 const path = require('path');
-
 module.exports = {
+    mode: 'production',
     entry: './js/dashboard_main.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
+        clean: true,
     },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/i, // Match CSS files
+                use: [
+                    'style-loader', // Inject CSS into DOM
+                    'css-loader', // Resolves CSS imports
+                ],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                test: /\.(png|jpe?g|gif|svg)$/i, // Match image files
+                use: [
+                    {
+                        loader: 'file-loader', // Use file-loader for images
+                        options: {
+                            name: '[name].[hash].[ext]', // Output file naming convention
+                            outputPath: 'images', // Output directory for images
+                        },
+                    },
+                ],
             },
         ],
     },
-    mode: 'production',
+    resolve: {
+        extensions: ['.js'],
+    },
 };
